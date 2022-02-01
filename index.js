aa05e9a3033c2c4663413da089ff0ebe9f45aa95
Inpuro <sopragames.sf@hotmail.com>
2022-02-01 19:31:59 -0300

    subindo alterações

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-const http = require("http");
+// const http = require("http");
 
-const server = http.createServer((request, response) => {
-  switch (request.url) {
-    case "/about":
-      response.write("About Hcode Treinamentos!");
-      break;
-    case "/":
-      response.write("Hello Hcode Treinamentos!");
-      break;
+// const server = http.createServer((request, response) => {
+//   switch (request.url) {
+//     case "/users":
+//       switch (request.method) {
+//         case "GET":
+//           response.write("List Users!");
+//           break;
+//         case "POST":
+//           response.write("Register Users!");
+//           break;
+//       }
+//       break;
+//     case "/":
+//       response.write("Node Server!");
+//       break;
+//     default:
+//       response.write("Not found page!");
+//       break;
+//   }
 
-    default:
-      response.write("Not found page!");
-      break;
-  }
+//   console.log("url", request.url);
+//   console.log("method", request.method);
 
-  console.log("url", request.url);
-  console.log("method", request.method);
+//   response.end();
+// });
 
-  response.end();
-});
+// server.listen(4000);
+//HTML Markup
+console.log("I");
 
-server.listen(3000);
+setTimeout(() => {
+  console.log("love")
+}, 0);
+
+console.log("JavaScript!");
